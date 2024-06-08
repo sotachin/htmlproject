4e72db4548263a1679a762b9d16b9e84b6a6b6fb
Sonita Chin <chin.sonita003@gmail.com>
2024-06-08 09:22:27 +0000

    Fix deploy script to force pulling image

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-docker service create --name sonita-html-app -p 8089:80 chinsonita/sonita-html-app:latest
+#docker service create --name sonita-html-app -p 8089:80 chinsonita/sonita-html-app:latest
+docker service update --force sonita-html-app
